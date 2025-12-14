f54f6087f89f2aca8ca26fe222d487d3c0078722
orion <49439069+orion-supernova@users.noreply.github.com>
2025-12-14 14:13:09 +0300

    shell-star 1.0.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/shell-star.rb
+++ Formula/shell-star.rb
@@ -1,13 +1,13 @@
 class ShellStar < Formula
   desc "WebSocket chat server powered by Vapor"
   homepage "https://github.com/YOURUSERNAME/shell-star"
-  version "1.0.5"
+  version "1.0.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/orion-supernova/shell-star/releases/download/v1.0.5/shell-star-1.0.5.tar.gz"
-      sha256 "df8d33dc2d01414861bdb5ec017a9e6d6a8c704d4f043cf204ec5b25258b535a"
+      url "https://github.com/orion-supernova/shell-star/releases/download/v1.0.6/shell-star-1.0.6.tar.gz"
+      sha256 "38da201ac87139e1da59c80894d153bdbb36a82a0eeaf48ad0b91a39ddae0653"
     else
       url "https://github.com/YOURUSERNAME/shell-star/releases/download/v1.0.0/shell-star-1.0.0.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FROM_RELEASE"
