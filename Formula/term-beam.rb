a75f5e45bcb53ec8dbb3cec99cd058550e9ea8d4
orion <49439069+orion-supernova@users.noreply.github.com>
2025-12-14 13:31:34 +0300

    term-beam 1.0.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/term-beam.rb
+++ Formula/term-beam.rb
@@ -1,12 +1,12 @@
 class TermBeam < Formula
   desc "Terminal chat client - beam messages across the network"
   homepage "https://github.com/orion-supernova/term-beam"
-  version "1.0.4"
+  version "1.0.5"
   license "MIT"
 
   on_macos do
-    url "https://github.com/orion-supernova/term-beam/releases/download/v1.0.4/term-beam-1.0.4.tar.gz"
-    sha256 "f43be83ea6173e0834492a7ea21688f1059aafde205fb24c1358a2295ed62ee0"
+    url "https://github.com/orion-supernova/term-beam/releases/download/v1.0.5/term-beam-1.0.5.tar.gz"
+    sha256 "e46f42463c15eb8fc14a7c641601e38f3ec451d66b9e2cf804f0d0671e0dcb0d"
   end
 
   on_linux do
